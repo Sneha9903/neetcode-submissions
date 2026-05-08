7a1ca2d8276e562cd7a9eb20551f95ac1353acdb
Sneha9903 <snehashaw9903@gmail.com>
2026-05-09 00:20:54 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length()>s2.length()) return false;
+        int[] arr = new int[26];
+        int[] arr2 = new int[26];
+        for (int i = 0; i<s1.length(); i++){
+            arr[s1.charAt(i)-'a']++;
+            arr2[s2.charAt(i)-'a']++;
+        }
+        int matches = 0;
+        for (int i =0; i<26; i++){
+            if (arr[i]==arr2[i]){
+                matches++;
+            }
+        }
+
+        int l = 0;
+        for (int r = s1.length(); r < s2.length(); r++) {
+            if (matches == 26) {
+                return true;
+            }
+
+            int index = s2.charAt(r) - 'a';
+            arr2[index]++;
+            if (arr[index] == arr2[index]) {
+                matches++;
+            } else if (arr[index] + 1 == arr2[index]) {
+                matches--;
+            }
+
+            index = s2.charAt(l) - 'a';
+            arr2[index]--;
+            if (arr[index] == arr2[index]) {
+                matches++;
+            } else if (arr[index] - 1 == arr2[index]) {
+                matches--;
+            }
+            l++;
+        }
+        return matches == 26;
+
+    }
+}
